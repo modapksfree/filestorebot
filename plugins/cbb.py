b08cc33aa50f45e477106814dc4973c5ed2f0309
modapksfree <modapksfreeio@gmail.com>
2024-09-26 15:12:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,7 @@ from pyrogram import __version__
 from bot import Bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-from config import PRICE1, PRICE2, PRICE3, PRICE4, PRICE5, UPI_ID, UPI_IMAGE_URL, SCREENSHOT_URL
+from config import PRICE1, PRICE2, PRICE3, PRICE4, PRICE5, SCREENSHOT_URL
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
@@ -28,12 +28,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             pass
     elif data == "buy_prem":
         await query.message.edit_text(
-            text=f"👋 {query.from_user.username}\n\n🎖️ Available Plans :\n\n● {PRICE1} rs For 7 Days Prime Membership\n\n● {PRICE2} rs For 1 Month Prime Membership\n\n● {PRICE3} rs For 3 Months Prime Membership\n\n● {PRICE4} rs For 6 Months Prime Membership\n\n● {PRICE5} rs For 1 Year Prime Membership\n\n\n💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment",
+            text=f"👋 {query.from_user.username}\n\n🎖️ Available Plans :\n\n● {PRICE1} rs For 7 Days Prime Membership\n\n● {PRICE2} rs For 1 Month Prime Membership\n\n● {PRICE3} rs For 3 Months Prime Membership\n\n● {PRICE4} rs For 6 Months Prime Membership\n\n● {PRICE5} rs For 1 Year Prime Membership\n\n\n<b> ᴄʟɪᴄᴋ ᴏɴ ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ ᴛᴏ ᴘᴜʀᴄʜᴀꜱᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴ 😊.<b>",
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                 [   
                     [
-                        InlineKeyboardButton("ꜱᴇɴᴅ ᴘᴀʏᴍᴇɴᴛ ꜱᴄʀᴇᴇɴꜱʜᴏᴛ(ᴀᴅᴍɪɴ) 📸", url=(SCREENSHOT_URL))
+                        InlineKeyboardButton("ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ 🎟️", url=(SCREENSHOT_URL))
                     ],
                     [
                         InlineKeyboardButton("ⓧ ᴄʟᴏꜱᴇ", callback_data = "close")
