170d330403e7ecb16b7f7722b53610c14dc2bcd1
modapksfree <modapksfreeio@gmail.com>
2024-09-26 13:50:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -218,8 +218,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data="about"),
-                    InlineKeyboardButton("🔒 Close", callback_data="close")
+                    InlineKeyboardButton("🦁 ᴀʙᴏᴜᴛ ᴍᴇ", callback_data="about"),
+                    InlineKeyboardButton("ⓧ ᴄʟᴏꜱᴇ", callback_data="close")
                 ]
             ]
         )
